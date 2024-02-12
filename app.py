8d81aa9fe6448756a8fe939d6c04663a784bea67
trinadh-34 <159611833+trinadh-34@users.noreply.github.com>
2024-02-12 16:54:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template
+from newsapi import NewsApiClient
+ 
+ 
+app = Flask(_name_)
+ 
+ 
+@app.route('/')
+def Index():
+    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+ 
+ 
+    mylist = zip(news, desc, img)
+ 
+ 
+    return render_template('index.html', context = mylist)
+
+
+@app.route('/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="YOUR-API-KEY")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+    mylist = zip(news, desc, img)
+ 
+    return render_template('bbc.html', context=mylist)
+ 
+if _name_=="_main_":
+    app.run(debug=True)
