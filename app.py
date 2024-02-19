a348ec0c922d13eb32a5a66c1d1a464635d242e6
trinadh-34 <159611833+trinadh-34@users.noreply.github.com>
2024-02-19 17:18:02 +0530

    Update app.py
    commited some changes that enables user to fetch news from the source bbc

--- app.py
+++ app.py
@@ -2,41 +2,14 @@ from flask import Flask, render_template
 from newsapi import NewsApiClient
  
  
-app = Flask(_name_)
- 
- 
+app = Flask(__name__)
+ 
+
 @app.route('/')
-def Index():
-    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
-    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
- 
-    articles = topheadlines['articles']
- 
-    desc = []
-    news = []
-    img = []
- 
- 
-    for i in range(len(articles)):
-        myarticles = articles[i]
- 
- 
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
- 
- 
- 
-    mylist = zip(news, desc, img)
- 
- 
-    return render_template('index.html', context = mylist)
-
-
-@app.route('/bbc')
 def bbc():
-    newsapi = NewsApiClient(api_key="YOUR-API-KEY")
-    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
+ #After registration on newsapi the apikey should be entered here
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")#Gets top headlines from bbc news
  
     articles = topheadlines['articles']
  
@@ -55,5 +28,5 @@ def bbc():
  
     return render_template('bbc.html', context=mylist)
  
-if _name_=="_main_":
+if __name__=="__main__":
     app.run(debug=True)
