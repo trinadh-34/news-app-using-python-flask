1f83076373b0706b3a40c2719efd2abc5ce1f6f0
trinadh-34 <159611833+trinadh-34@users.noreply.github.com>
2024-02-25 17:17:25 +0530

    update app.py
    updated app.py code so that the http get request method is used instead of using NewsApiClient module

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-from flask import Flask, render_template
-from newsapi import NewsApiClient
+from flask import Flask,render_template
+import requests
  
+api_key="b0f75ce660c0466a9a98c2478f8abb62" #enter your api key here
  
 app = Flask(__name__)
- 
-
-@app.route('/')
-def bbc():
-    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
- #After registration on newsapi the apikey should be entered here
-    topheadlines = newsapi.get_top_headlines(sources="bbc-news")#Gets top headlines from bbc news
  
-    articles = topheadlines['articles']
+
+ 
+@app.route('/')
+
+def bbc():
+    main_url =f"https://newsapi.org/v2/top-headlines?country=in&apiKey="+api_key # fetches top headlines from india 
+    news = requests.get(main_url).json()  # renders result in a json format                                                          
+    article = news['articles']  #get the articles from the topheadlines 
  
     desc = []
     news = []
     img = []
+    link=[]
+    myarticle=[]
  
-    for i in range(len(articles)):
-        myarticles = articles[i]
+   for i in range(len(article)): #iterate over the articles key fetched from news["articles"]
+        myarticle = article[i]
  
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
  
-    mylist = zip(news, desc, img)
- 
-    return render_template('bbc.html', context=mylist)
- 
+        news.append(myarticle['title'])
+        desc.append(myarticle['description'])
+        img.append(myarticle['urlToImage'])
+        link.append(myarticle['url'])
+    my_list = zip(news, desc, img,link)  # zip method yield n length tuples  where n represents the positional arguments 
+
+    return render_template('bbc.html', context =my_list)
+
 if __name__=="__main__":
     app.run(debug=True)
